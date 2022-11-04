6adcdfb94487f0b8f9b2deb1e75b8c0b4d170b4d
MacYourBae <83893401+MacYourBae@users.noreply.github.com>
2022-11-04 16:35:29 +0900

    Add files via upload

---
+++ storage/framework/views/2490c66d398df8981ecddccdcf5230b2f8b3bd2b.php
@@ -0,0 +1,11 @@
+<?php echo $__env->make('navbar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+
+
+<?php $__env->startSection('title' , 'home'); ?>
+
+<?php $__env->startSection('content'); ?>
+    <img src="https://www.amd.com/system/files/2021-05/871550-FarCry6-key-art-1920x600.jpg" class="img-fluid" alt="Responsive image">
+    <img src="https://i.pinimg.com/originals/21/dd/8f/21dd8fc241de94233c0baa832b917dbc.jpg" class="img-fluid" alt="Responsive image">
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('template', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Application\Laravel\ASG\GSLC05\resources\views/home.blade.php ENDPATH**/ ?>
